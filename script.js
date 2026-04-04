171a6072e69cd13dd994b716dcdc751584a21150
Jim Wilbur <45021912+JimboWW@users.noreply.github.com>
2026-04-03 17:19:06 -0700

    Add click event listener to toggle button class

---
+++ script.js
@@ -0,0 +1,11 @@
+const buttons = document.querySelectorAll(".button");
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        button.classList.toggle("active");
+
+    });
+
+});
